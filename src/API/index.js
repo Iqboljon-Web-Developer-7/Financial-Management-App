27be34f5b68f4d7fc38dcb1968341ee4201b27a2
Iqboljon Ummataliyev <iqboljonummataliyev30@gmail.com>
2024-12-16 23:59:43 +0500

    Update index.js

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_CURRENCY_API_KEY;
-console.log();
+
 
 const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/`;
 export const axiosFun = axios.create({
